f3388384b5e25c465d20d066fe4d90b74ddd3cfe
Glitch (nainoboot) <none>
2020-01-03 20:33:56 +0000

    Maroc

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# notnotnot
+# ✨ Naino Bot ✨
+   ✨the king bot✨
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NjU5NDcyMTk3NzgxNTUzMTUy.XgOzbA.d0dDWjTivmpWt5wsn2xT-yyLIvc',    
+    YT_API_KEY: 'api ',
+    prefix: '!',
+    devs: ['369070401516470284']
+}
